67359539445f56f05b91b79ecbaddd7e9c58b053
Dmitry Kuchukov <kudim@inbox.ru>
2024-07-06 19:11:00 +0300

    feat: add layout serach

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
+import React from 'react';
 import './App.css';
+import Search from './components/Search';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div>
-      <h1>Start Page</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  handleClickPlus = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleClickMinus = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  componentDidMount(): void {
+    console.log('component did mount');
+  }
+  componentDidUpdate(): void {
+    console.log('component did update');
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <Search />
+      </div>
+    );
+  }
 }
 
 export default App;
