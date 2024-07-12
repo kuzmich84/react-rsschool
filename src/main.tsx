534c01309ef7cfbcf0b295f939d1643eab598f4b
Dmitry Kuchukov <kudim@inbox.ru>
2024-07-12 20:53:57 +0300

    feat:add react-roter, page 404

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './components/NotFound/NotFound.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    ),
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
