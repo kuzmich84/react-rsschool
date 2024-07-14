cdecbc8c4efa7b17ded277239cc29a8e5fc5ba55
Dmitry Kuchukov <kudim@inbox.ru>
2024-07-14 11:55:27 +0300

    feat: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {},
   plugins: [react()],
   base: './',
 });
