ec531ea30418a561074114e61c436facf2b9be3d
Dmitry Kuchukov <kudim@inbox.ru>
2024-07-21 10:39:59 +0300

    refactor: add function component Header instead of class component.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
 import BoundaryButtonTest from '../ui/BoundaryButtonTest';
 import styles from './Header.module.css';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <>
-        <div className={styles.header_content}>
-          <h1>Movie Search</h1>
-          <BoundaryButtonTest />
-        </div>
-        <hr />
-      </>
-    );
-  }
+function Header() {
+  return (
+    <>
+      <div className={styles.header_content}>
+        <h1>Movie Search</h1>
+        <BoundaryButtonTest />
+      </div>
+      <hr />
+    </>
+  );
 }
 
 export default Header;
