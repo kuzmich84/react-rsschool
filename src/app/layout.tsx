e0ddb2772135775e640b0bcba653041be43dd8f1
Dmitry Kuchukov <kudim@inbox.ru>
2024-07-31 21:34:48 +0300

    feat: install nextjs

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+        <meta name="description" content="My App is a search movies." />
+        <title>Movie Search</title>
+      </head>
+
+      <body>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  );
+}
